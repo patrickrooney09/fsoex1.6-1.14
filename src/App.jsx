095e8cbc52cbf5dfff27f4300dea27b7bc60a842
Patrick Rooney <patrickrooney09@gmail.com>
2023-08-28 16:42:32 -0400

    stats updating properly

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,54 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  const findAll = (bad, neutral, good) => {
-    return bad + neutral + good;
-  };
-
-  const findAverage = (bad, neutral, good) => {
-    return (good - bad) / (bad + neutral + good);
-  };
+  const [total, setTotal] = useState(0);
+  const [average, setAverage] = useState(0);
 
   return (
     <div>
-      <button onClick={() => setGood(good + 1)}>Good {good}</button>
-      <button onClick={() => setNeutral(neutral + 1)}>Neutral {neutral}</button>
-      <button onClick={() => setBad(bad + 1)}>Bad {bad}</button>
+      <h1>Give Feedback</h1>
+      <button
+        onClick={() => {
+          const updatedGood = good + 1;
+          const updatedTotal = total + 1;
+          setGood(updatedGood);
+          setTotal(updatedTotal);
+          setAverage((updatedGood - bad) / updatedTotal);
+        }}
+      >
+        Good
+      </button>
+      <button
+        onClick={() => {
+          const updatedNeutral = neutral + 1;
+          const updatedTotal = total + 1;
+          setNeutral(updatedNeutral);
+          setTotal(updatedTotal);
+          setAverage((good - bad) / updatedTotal);
+        }}
+      >
+        Neutral
+      </button>
+      <button
+        onClick={() => {
+          const updatedBad = bad + 1;
+          const updatedTotal = total + 1;
+          setBad(updatedBad);
+          setTotal(updatedTotal);
+          setAverage((good - updatedBad) / updatedTotal);
+        }}
+      >
+        Bad
+      </button>
+      <h1>Stats</h1>
+      <div>
+        <div>Good: {good}</div>
+        <div>Neutral: {neutral}</div>
+        <div>Bad: {bad}</div>
+
+        <div>Total:{total}</div>
+        <div>Average:{average}</div>
+      </div>
     </div>
   );
 };
