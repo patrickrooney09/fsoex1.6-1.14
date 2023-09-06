83844316b3d6254012f715620989612dba336c93
Patrick Rooney <patrickrooney09@gmail.com>
2023-09-06 13:38:45 -0400

    1.13 done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,20 @@ import Statistics from "./Statistics";
 import Button from "./Button";
 
 const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [votes, setVotes] = useState(new Uint8Array(anecdotes.length));
+
+  // increment the property 2 value by one
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -16,20 +30,11 @@ const App = () => {
   function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  const anecdotes = [
-    "If it hurts, do it more often.",
-    "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    "Premature optimization is the root of all evil.",
-    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
-    "The only way to go fast, is to go well.",
-  ];
-
+  console.log(selected);
   return (
     <div>
       <div>{anecdotes[selected]}</div>
+      <div>Votes {votes[selected]}</div>
       <button
         onClick={() => {
           const random = getRandomInt(0, anecdotes.length - 1);
@@ -37,6 +42,15 @@ const App = () => {
         }}
       >
         Next Anecdote
+      </button>
+      <button
+        onClick={() => {
+          let copy = [...votes];
+          copy[selected] += 1;
+          setVotes(copy);
+        }}
+      >
+        Vote
       </button>
       <h1>Give Feedback</h1>
       <Button
